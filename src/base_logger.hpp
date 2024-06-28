67bc2ea2d9a3ac2e226a6c3dfed8dfb9c82300a5
Arnoldas Gerulskis <arnoldas.g@limemicro.lt>
2024-06-28 11:05:35 +0300

    Removed base_logger static property.

--- src/base_logger.hpp
+++ src/base_logger.hpp
@@ -13,15 +13,14 @@ namespace uni_logger
 
 class base_logger
 {   
-    public:
+    private:
 
     // Add a new log file entry.
-    static int new_log(std::string file_name, std::string dir = L_HOME_DIR);
+    int new_log(std::string file_name, std::string dir = L_HOME_DIR);
 
     // Remove a log file entry.
-    static int remove_log(std::string file_name);
+    int remove_log(std::string file_name);
 
-    private:
     std::vector<std::ofstream> m_log_files;
     std::map<std::string, int> m_log_files_index;
 };
